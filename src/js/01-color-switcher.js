c72cd102df70cf07dc0c12d6eb88bb4eb5d83376
Kop1tes <prsasha0@gmail.com>
2022-12-08 12:57:12 +0200

    add first change

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let interval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+btnStart.addEventListener('click', () => {
+    interval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(interval);
+});
